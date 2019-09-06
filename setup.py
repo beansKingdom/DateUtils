4c9e76697ab970bd5c1976a878ca42109e272d17
hly <helingyun02@meituan.com>
2019-09-06 14:22:15 +0800

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BeanDateUtils",
-    version="0.3",
+    version="0.4",
     author="Hly",
     author_email="hlyaction@gmail.com",
     description="A Python library for date compute.",
